01c7480954d8f344bf5fb6537970d839baddcb97
Paolo Matthew Tolentino <tolentinopaolomatthew23@gmail.com>
2021-12-16 17:13:43 +0800

    initial setup for redux state management

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,9 +3,11 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NativeBaseProvider } from "native-base";
 import { registerRootComponent } from "expo";
+import { Provider as ReduxProvider } from "react-redux";
 
 import { LoginPage } from "../login/LoginPage";
 import { SignUpPage } from "../signup/SignUpPage";
+import { store } from "./store";
 
 export type StackParamsList = {
   login: undefined;
@@ -16,18 +18,20 @@ const Stack = createNativeStackNavigator<StackParamsList>();
 
 function App() {
   return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="login">
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="login"
-            component={LoginPage}
-          />
-          <Stack.Screen name="sign-up" component={SignUpPage} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
+    <ReduxProvider store={store}>
+      <NativeBaseProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="login">
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="login"
+              component={LoginPage}
+            />
+            <Stack.Screen name="sign-up" component={SignUpPage} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </ReduxProvider>
   );
 }
 export default registerRootComponent(App);
